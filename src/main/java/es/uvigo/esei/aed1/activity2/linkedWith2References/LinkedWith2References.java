a10d178e93b9416ec16f15124d877ff185127635
gfrodinho_esei.uvigo <gfrodinho_esei.uvigo@DESKTOP-8UGNBHN>
2025-02-21 12:33:07 +0100

    LinkedWith2References: Implementación finalizada

--- src/main/java/es/uvigo/esei/aed1/activity2/linkedWith2References/LinkedWith2References.java
+++ src/main/java/es/uvigo/esei/aed1/activity2/linkedWith2References/LinkedWith2References.java
@@ -15,7 +15,7 @@ public class LinkedWith2References {
     }
 
     public boolean isEmpty() {
-        return (this.elementsCount == 0 && first == null && last == null);
+        return (first == null && last == null && elementsCount == 0);
     }
 
     public int size() {
@@ -41,17 +41,9 @@ public class LinkedWith2References {
     }
 
     public void addFirst(int value) {
-        Node nextNode;
-
-        if (this.isEmpty()) {
-            nextNode = null;
-        } else {
-            nextNode = this.first;
-        }
-
-        Node newNode = new Node(value, nextNode);
+        Node newNode = new Node(value, this.first);
         this.first = newNode;
-        if (this.isEmpty()) {
+        if (this.elementsCount == 1) {
             this.last = newNode;
         }
         this.elementsCount++;
@@ -67,37 +59,30 @@ public class LinkedWith2References {
             this.last = newNode;
             this.elementsCount++;
         }
-
     }
 
     public void remove(int value) throws IllegalAccessException {
         if (this.isEmpty()) {
             throw new IllegalAccessException("The structure is empty. Values cannot be deleted.");
         } else {
-            if (this.first.hasValue(value)) { // Comprobamos el primer elemento y lo eliminamos
-                if (this.size() == 1) { // Cuando solo hay un elemento
-                    this.elementsCount--;
-                    this.first = null;
-                    this.last = null;
-                } else { // Cuando hay más de un elemento
-                    this.first = this.first.getNext();
-                    this.elementsCount--;
-                }
+            if (this.first != null && this.first.hasValue(value)) { // Comprobamos el primer elemento y lo eliminamos
+                this.first = this.first.getNext();
+                this.elementsCount--;
             } else { // Buscamos el elemento a borrar
                 Node current = this.first;
                 while (current.getNext() != null && !current.getNext().hasValue(value)) {
                     current = current.getNext();
                 }
-                
-                if(current.getNext() == null) {
+
+                if (current.getNext() == null) { // We reached the end of the list
                     throw new IllegalAccessException("The value is not contained in the structure");
                 } else {
                     //We are about to delete an element, but we first check if it is the last one
                     boolean isLastElement = (current.getNext() == this.last);
-                    
-                    if(isLastElement) {
+
+                    if (isLastElement) {
                         this.last = current;
-                    }  else {
+                    } else {
                         current.setNext(current.getNext().getNext());
                     }
                     this.elementsCount--;
@@ -109,11 +94,11 @@ public class LinkedWith2References {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        
-        for(Node current = this.first; current != null; current = current.getNext()) {
+
+        for (Node current = this.first; current != null; current = current.getNext()) {
             sb.append(current.getValue()).append(" ");
         }
-        
+
         return sb.toString();
     }
 
