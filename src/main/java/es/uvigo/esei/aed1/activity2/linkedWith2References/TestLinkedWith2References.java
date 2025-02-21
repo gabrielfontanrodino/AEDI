9cf4219f96ce12ffe64ba38ae4f6af2988b2509a
Gabriel Fontán <gabilessto@gmail.com>
2025-02-21 13:08:11 +0100

    Refactor TestLinkedWith2References to improve value removal logic and enhance readability

--- src/main/java/es/uvigo/esei/aed1/activity2/linkedWith2References/TestLinkedWith2References.java
+++ src/main/java/es/uvigo/esei/aed1/activity2/linkedWith2References/TestLinkedWith2References.java
@@ -1,38 +1,39 @@
-
 package es.uvigo.esei.aed1.activity2.linkedWith2References;
 
 public class TestLinkedWith2References {
 
-  public static void main(String[] args) {
-    LinkedWith2References list = new LinkedWith2References();
-    
-    for (int i = 1; i <= 10; i++) {
-      list.addLast(i);
+    public static void main(String[] args) {
+        LinkedWith2References list = new LinkedWith2References();
+
+        for (int i = 1; i <= 10; i++) {
+            list.addLast(i);
+        }
+
+        for (int i = 0; i <= 12; i = i + 2) {
+            list.addFirst(i);
+        }
+
+        System.out.println("is empty?: " + list.isEmpty());
+        System.out.println("The number of values is: " + list.size());
+        System.out.println("The value 10 is contained:  " + list.numberOfOccurrences(10) + " times");
+        System.out.println("Is contained the 15? : " + list.contains(15));
+
+        int occurrences = list.numberOfOccurrences(10);
+        for(int i = 0; i < occurrences; i++) {
+            System.out.println("Removing the value 10...");
+            removeValue(list, 10);
+            System.out.println("The value 10 is contained:  " + list.numberOfOccurrences(10) + " times");
+        }
+
+        System.out.println("List: " + list);
     }
 
-    for (int i = 0; i <= 12; i = i + 2) {
-      list.addFirst(i);
+    private static void removeValue(LinkedWith2References list, int value) {
+        try {
+            list.remove(value);
+        } catch (IllegalAccessException e) {
+            System.out.println(e.getMessage());
+        }
     }
-
-    System.out.println("is empty?: " + list.isEmpty());
-    System.out.println("The number of values is: " + list.size());
-    System.out.println("The value 10 is contained:  " + list.numberOfOccurrences(10) + " times");
-    System.out.println("Is contained the 15? : " + list.contains(15));
-    System.out.println("Remove value 10... ");
-    try {
-         list.remove(10);   
-    } catch (IllegalAccessException e) {
-        System.out.println(e.getMessage());
-    }
-    System.out.println("The value 10 is contained:  " + list.numberOfOccurrences(10) + " times");
-    System.out.println("Remove value 10... ");
-    try {
-         list.remove(10);   
-    } catch (IllegalAccessException e) {
-        System.out.println(e.getMessage());
-    }
-    System.out.println("The value 10 is contained:  " + list.numberOfOccurrences(10) + " times");
-    System.out.println("List: " + list);
-  }
-
 }
+
