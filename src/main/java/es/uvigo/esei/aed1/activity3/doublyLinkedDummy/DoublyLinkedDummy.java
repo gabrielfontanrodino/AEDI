a5e46a8df5e60f28c435d3d79782c1c1d2b84088
Gabriel Fontán <gabilessto@gmail.com>
2025-03-15 17:38:12 +0100

    Implement core functionalities in DoublyLinkedDummy and add SimpleLinkedDummy class with basic operations

--- src/main/java/es/uvigo/esei/aed1/activity3/doublyLinkedDummy/DoublyLinkedDummy.java
+++ src/main/java/es/uvigo/esei/aed1/activity3/doublyLinkedDummy/DoublyLinkedDummy.java
@@ -1,18 +1,26 @@
 package es.uvigo.esei.aed1.activity3.doublyLinkedDummy;
 
+import es.uvigo.esei.aed1.commonLinked.DoubleNode;
+
 public class DoublyLinkedDummy {
 
+    private DoubleNode first;
+    private DoubleNode last;
+    private int elementsCount;
 
     public DoublyLinkedDummy() {
-
+        this.first = new DoubleNode(null, -0, null);
+        this.last = new DoubleNode(this.first, -0, null);
+        this.first.setNext(this.last);
+        this.elementsCount = 0;
     }
 
     public boolean isEmpty() {
-        return true;
+        return this.elementsCount == 0;
     }
 
     public int size() {
-        return 0;
+        return this.elementsCount;
     }
 
     public int numberOfOccurrences(int value) {
@@ -32,7 +40,22 @@ public class DoublyLinkedDummy {
     }
 
     public void remove(int value) {
+        if(this.isEmpty()) {
+            System.out.println("The list is empty");
+        } else {
+            DoubleNode current = this.first.getNext();
+            while(current.getNext() != null && !current.hasValue(value)) {
+                current = current.getNext();
+            }
 
+            if(current == this.last) {
+                System.out.println("The value " + value + " is not in the list");
+            } else {
+                current.getPrevious().setNext(current.getNext());
+                current.getNext().setPrevious(current.getPrevious());
+                this.elementsCount--;
+            }
+        }
     }
 
     @Override
---
+++ src/main/java/es/uvigo/esei/aed1/activity3/simpleLinkedDummy/SimpleLinkedDummy.java
@@ -0,0 +1,88 @@
+package es.uvigo.esei.aed1.activity3.simpleLinkedDummy;
+
+import es.uvigo.esei.aed1.commonLinked.Node;
+
+public class SimpleLinkedDummy {
+    private Node first;
+    private int elementsCount;
+
+    public SimpleLinkedDummy() {
+        this.first = new Node(-0, null);
+        this.elementsCount = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.elementsCount == 0;
+    }
+
+    public int size() {
+        return this.elementsCount;
+    }
+
+    public int numberOfOccurrences(int value) {
+        int count = 0;
+
+        for(Node current = this.first.getNext(); current != null; current = current.getNext()) {
+            if(current.hasValue(value)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public boolean contains(int value) {
+//        for(Node current = this.first.getNext(); current != null; current = current.getNext()) {
+//            if(current.hasValue(value)) {
+//                return true;
+//            }
+//        }
+//
+//        return false;
+        Node current = this.first.getNext();
+
+        while(current != null && !current.hasValue(value)) {
+            current = current.getNext();
+        }
+
+        return current != null;
+    }
+
+    public void addFirst(int value) {
+        Node newNode = new Node(value, this.first.getNext());
+        this.first.setNext(newNode);
+        this.elementsCount++;
+    }
+
+    public void addLast(int value) {
+        Node newNode = new Node(value, null);
+
+        Node current = this.first;
+        while(current.getNext() != null) {
+            current = current.getNext();
+        }
+
+        current.setNext(newNode);
+        this.elementsCount++;
+    }
+
+    public void remove(int value) {
+        if(this.isEmpty()) {
+            System.out.println("The list is empty");
+        } else {
+            Node current = this.first;
+
+            while(current.getNext() != null && !current.getNext().hasValue(value)) { //Con un for se puede hacer más fácil
+                current = current.getNext();
+            }
+
+            if(current.getNext() != null) { //Si el elemento siguiente no es nulo (es decir, si estamos en el último elemento de la lista)
+                current.setNext(current.getNext().getNext()); // Desenlazamos el nodo que queremos eliminar y el GC se encargará de eliminarlo
+                this.elementsCount--;
+            } else {
+                System.out.println("The value " + value + " is not in the list");
+            }
+        }
+    }
+
+}
